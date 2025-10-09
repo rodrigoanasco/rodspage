81642d9ad6896050c7a3c0ff47abcf7feaeab5f3
rodrigoanasco <ro.anasco.s@gmail.com>
2025-10-08 23:53:27 -0700

    Something

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
         <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
             <p className="sm:text-3xl text-2xl font-medium text-white text-center font-mono">
                 Hello, I'm Rodrigo! 
-                <span className="waving-hand">🤙</span>
+                <span className="waving-hand">🤙🎉</span>
             </p>
             
             <p className="hero_tag text-gray_gradient text-center">
