d3dbe3621387587030d0f920f5199b8b4d8f768c
rodrigoanasco <ro.anasco.s@gmail.com>
2025-10-31 07:45:53 -0700

    started project section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -84,7 +84,7 @@ const About = () => {
                         Contact me
                     </p>
                     <div className='copy-container' onClick={handleCopy}>
-                        <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt='copy'/>
+                        <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt='copy'/>
                         <p className='lg:text-2xl md:text-xl font-medium text-gray_gradient text-white'>ro.anasco.s@gmail.com</p>
                     </div>
                 </div>
