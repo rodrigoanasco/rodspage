192ada983399991963abbdab49fe11119e286ccc
rodrigoanasco <ro.anasco.s@gmail.com>
2025-10-31 07:35:20 -0700

    finished the prototype of the about me section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import {useState} from 'react'
+import Globe from 'react-globe.gl'
+import Button from '../components/Button'
 
 const About = () => {
-  return (
+    const [hasCopied, setHasCopied] = useState(false);
+    
+    const handleCopy = () => {
+        navigator.clipboard.writeText('ro.anasco.s@gmail.com')
+        setHasCopied(true);
+        setTimeout(() => {setHasCopied(false);}, 2000);
+    }
+  
+    return (
     <section className='c-space my-20'>
         <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
             <div className='col-span-1 xl:row-span-3'>
@@ -14,7 +24,7 @@ const About = () => {
                 </div>
             </div>
 
-        <div className='col-span1 xl:row-span-3'>
+        <div className='col-span-1 xl:row-span-3'>
             <div className='grid-container'>
                 <img src="/assets/grid2.png" alt='grid-2' className='w-full sm:w-[276px] h-fit object-contain'/>
                 <div>
@@ -27,11 +37,60 @@ const About = () => {
         <div className='col-span-1 xl:row-span-4'>
             <div className='grid-container'>
                 <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-
+                    <Globe 
+                    height={326}
+                    width={326}
+                    backgroundColor='rgba(0,0,0,0)'
+                    backgroundImageOpacity={0.5}
+                    showAtmosphere
+                    showGraticules
+                    globeImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                    bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                    />
+                </div>
+                <div>
+                    <p className='grid-headtext'>
+                        Currently living in Vancouver
+                    </p>
+                    <p className='grid-subtext'>
+                        But available to reallocate for work!
+                    </p>
+                    <Button name="Contact Me" isBeam containerClass="w-full mt-10"/>
                 </div>
             </div>
-
         </div>
+
+        <div className='xl:col-span-2 xl:row-span-3'>
+            <div className='grid-container'>
+                <img src='/assets/grid3.png' alt='grid-3'
+                className='w-full sm:h-[266px] h-fit object-contain'/>
+
+                <div>
+                    <p className='grid-headtext'>
+                        My Passion for Coding
+                    </p>
+                    <p className='grid-subtext'>
+                        I love coding problems and building things through code...
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div className='xl:col-span-1 xl:row-span-2'>
+            <div className='grid-container'>
+                <img src='assets/grid4.png' alt='grid-4' className='w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top'/>
+                <div className='space-y-2'>
+                    <p className='grid-subtext text-center'>
+                        Contact me
+                    </p>
+                    <div className='copy-container' onClick={handleCopy}>
+                        <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt='copy'/>
+                        <p className='lg:text-2xl md:text-xl font-medium text-gray_gradient text-white'>ro.anasco.s@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         </div>
     </section>
   )
