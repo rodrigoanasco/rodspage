458c3b6535301eedc6310936965605c17f82c5f8
rodrigoanasco <ro.anasco.s@gmail.com>
2025-11-05 11:17:04 -0800

    Finished UI in projects section
    missing updating the videos for each project and the comments for each

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -71,19 +71,17 @@ const Projects = () => {
             
             <div className='border border-black-300 bg-black-200 rounded-lg h-96 md:h-full'>
                 <Canvas>
-                    <ambientLight intensity={1}/>
+                    <ambientLight intensity={Math.PI}/>
                     <directionalLight position={[10, 10, 5]} />
                     <Center>
                         <Suspense fallback={<CanvasLoader />}>
                             <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
-                                <DemoComputer/>
+                                <DemoComputer texture={currentProject.texture}/>
 
                             </group>
                         </Suspense>
                     </Center>
-                    <OrbitControls>
-
-                    </OrbitControls>
+                    <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false}/>
                 </Canvas>
             </div>
         </div>
