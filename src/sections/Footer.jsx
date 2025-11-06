30378d0045b303ffaf9a38029042620e2fb20f27
rodrigoanasco <ro.anasco.s@gmail.com>
2025-11-05 17:04:36 -0800

    fixed footer links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -12,15 +12,15 @@ const Footer = () => {
             </div>
             
             <div className='flex gap-3'>
-                <div className='social-icon'>
+                <a href="https://github.com/rodrigoanasco" target="_blank" rel="noopener noreferrer" className="social-icon">
                     <img src="/assets/github.svg" alt="github" className='w-1/2 h-1/2'/>
-                </div>
-                <div className='social-icon'>
+                </a>
+                <a href="https://www.instagram.com/rodr_1201/" target="_blank" rel="noopener noreferrer" className="social-icon">
                     <img src="/assets/instagram.svg" alt="instagram" className='w-1/2 h-1/2'/>
-                </div>
-                <div className='social-icon'>
+                </a>
+                <a href="https://www.linkedin.com/in/rodrigo-anasco" target="_blank" rel="noopener noreferrer" className="social-icon">
                     <img src="/assets/linkedin.svg" alt="linkedin" className='w-1/2 h-1/2'/>
-                </div>
+                </a>
             </div>
 
             <p className='text-white-500'>© 2025 Rodrigo Anasco. All rights reserved</p>
